9ee5fbcc38d7f328e5f887e3a88b9625a442c732
Brase <brasea@wwt.com>
2022-07-29 13:23:18 -0500

    set up practice area for JS

--- 01 setup/practice.js
+++ 01 setup/practice.js
@@ -7,3 +7,14 @@ a mult
 line
 comment */
 
+// more ways to declare a variable
+var hisName = "bob"
+let myName = "andrew"
+const pi = 3.14
+
+/*
+difference between var let and const
+VAR is going to be able to be used throughout the entire program
+LET will only be used within the scope of where it was declared
+CONST is a variable that cannot change / should not change
+*/
