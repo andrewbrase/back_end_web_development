a1ca192725737ca9efacb05811086f6419d8d404
Brase <brasea@wwt.com>
2022-07-29 13:19:55 -0500

    set up practice area for JS

---
+++ 01 setup/practice.js
@@ -0,0 +1,9 @@
+/* a lot of my previous Javascript practice has been w/ the frontend
+folder - this is to catch up and practice and new things that I didn't previously know
+*/
+
+/* this is 
+a mult
+line
+comment */
+
