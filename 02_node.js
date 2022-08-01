ca8e0902881db821b2e9e28bbd2b18c370a4e84e
Brase <brasea@wwt.com>
2022-07-31 20:45:40 -0500

    learned how to require a module and also organized files

--- 02_node.js
+++ 02_node.js
@@ -1,7 +1,15 @@
 const hello = "hello world";
 console.log(hello);
 
-// to run file with node.js - node 02_node.js
-// ---> hello world
+/* to run file with node.js - node 02_node.js
+---> hello world
 
-// in order to read files from the file system we need to use a module
+in order to read files from the file system we need to use a module
+that is with the fs module require them in the code to use the 
+module and then store the result of the requiring function in a 
+variable */
+
+// (fs stands for file system)
+const fs = require('fs');
+
+// this is the documentation on node - https://nodejs.org/en/docs/
