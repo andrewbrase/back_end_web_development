0bc022985341183dc6b82f2ef56e13595b1e0d64
Brase <brasea@wwt.com>
2022-07-31 20:36:46 -0500

    started hello world with node

---
+++ 02_node.js
@@ -0,0 +1,7 @@
+const hello = "hello world";
+console.log(hello);
+
+// to run file with node.js - node 02_node.js
+// ---> hello world
+
+// in order to read files from the file system we need to use a module
