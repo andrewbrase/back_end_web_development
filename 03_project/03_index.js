d33b29b1bef58a691dce2720cffe57689e28ec60
Brase <brasea@wwt.com>
2022-08-02 16:46:36 -0500

    started 03_project

--- 03_project/03_index.js
+++ 03_project/03_index.js
@@ -4,13 +4,25 @@ const http = require('http');
 const url = require('url');
 
 // assigning html pages to consts
-const homepage = fs.readFileSync('03_home.html');
-const productpage = fs.readFileSync('03_market.html');
-const otherpage = fs.readFileSync('03_other.html');
+const homepage = fs.readFileSync('03_home.html', 'utf-8');
+const productpage = fs.readFileSync('03_market.html', 'utf-8');
+const otherpage = fs.readFileSync('03_other.html', 'utf-8');
+const tempCard = fs.readFileSync('03_temp_card.html', 'utf-8');
 
 // parsing the JSON data to dataObj to be used w/ JS
 const data = fs.readFileSync(`${__dirname}/03_json.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+// replace template function - takes in a template and a product
+const replaceTemplate = (temp,product) => {
+    // replaces whatever was the placeholder for the product from json file .product
+    let output = temp.replace(/{%PROD%}/g, product.product);
+    output = output.replace(/{%COST%}/g, product.cost);
+    output = output.replace(/{%COND%}/g, product.condition);
+    output = output.replace(/{%MANU%}/g, product.manufacturer);
+    output = output.replace(/{%WORK%}/g, product.works);
+    return output;
+}
 
 // creating server
 const server = http.createServer((req, res) => {
@@ -31,8 +43,13 @@ if(pathName === "/" || pathName === "/home") {
         'Content-type' : 'text/html'
     });
 
-    //
-    res.end(productpage)
+    // looping over an array , each element will return something -> 
+    // that something will be put into a position but in the new card array
+    // this will replace the placeholders
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = productpage.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output)
 
 // other page
 } else if (pathName === '/other') {
