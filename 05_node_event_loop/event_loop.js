1f97f9bcfb05e34be33f25e53134400a82f2e23d
Brase <brasea@wwt.com>
2022-08-07 14:56:34 -0500

    learned about the threadpool

--- 05_node_event_loop/event_loop.js
+++ 05_node_event_loop/event_loop.js
@@ -75,7 +75,46 @@ L________________________________________________________|  yes
 
 */
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// const fs = require('fs');
+
+// setTimeout(() => console.log('timer 1 finished'), 0 );
+// setImmediate(() => console.log('immediate 1 finished'));
+
+// fs.readFile("event_text.txt", () => {
+//     console.log('I/O finished');
+
+//     setTimeout(() => console.log('timer 2 finished'), 0 );
+//     setTimeout(() => console.log('timer 3 finished'), 3000 );
+//     setImmediate(() => console.log('immediate 2 finished'));
+
+//     process.nextTick(() => console.log('process.nextTick'));
+// });
+
+// console.log('hello from the top level code');
+
+/*
+--->
+hello from the top level code
+timer 1 finished
+immediate 1 finished
+I/O finished
+
+process.nextTick (part of the microtasks que which gets executed 
+        after EVERY phase, not just after every full around tick)
+        set immediate gets executed once per tick, next tick gets executed immediately
+immediate 2 finished
+timer 2 finished
+timer 3 finished
+*/
+
+// we're going to use cryptography to encrypt a password to show off the
+// thread pool, it's amore complex operation that will be offloaded
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start_time = Date.now();
 
 setTimeout(() => console.log('timer 1 finished'), 0 );
 setImmediate(() => console.log('immediate 1 finished'));
@@ -87,7 +126,11 @@ fs.readFile("event_text.txt", () => {
     setTimeout(() => console.log('timer 3 finished'), 3000 );
     setImmediate(() => console.log('immediate 2 finished'));
 
-    process.nextTick(() => console.log('process.nextTick'))
+    process.nextTick(() => console.log('process.nextTick'));
+
+    crypto.pbkdf2('password','salt1234',100000, 1024, 'sha512', () => {
+        console.log(Date.now() - start_time , 'password encrypted');
+    });
 });
 
 console.log('hello from the top level code');
@@ -98,10 +141,17 @@ hello from the top level code
 timer 1 finished
 immediate 1 finished
 I/O finished
-
-process.nextTick (part of the microtasks que which gets executed 
-        after EVERY phase, not just after every full around tick)
+process.nextTick
 immediate 2 finished
 timer 2 finished
+
+// there are 4 thread pools, if you had 4 passwords to encrypt they'd
+all be done at the same time
+, you can actually change the thread pool size by using - process.env.UV_THREADPOOL_SIZE = 1;
+// if you ran the 4 password encryptions, theyd have to wait for the previous password
+to be encrypted, since the thread pool is only 1
+
+// it took 1034 milliseconds to encrypt
+1034 password encrypted
 timer 3 finished
 */
