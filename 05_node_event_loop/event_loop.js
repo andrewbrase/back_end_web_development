d013c4f6214f358f82a710d8aff15ed54653d89e
Brase <brasea@wwt.com>
2022-08-06 23:22:14 -0500

    started learning about how node js event loop works

--- 05_node_event_loop/event_loop.js
+++ 05_node_event_loop/event_loop.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/* const fs = require('fs');
 
 // set a timer to log to the console after 0 seconds
 setTimeout(() => console.log('timer 1 finished'), 0 );
@@ -10,3 +10,36 @@ fs.readFile('event_text.txt', () => {
 
 // this is the only one that is not inside of a callback
 console.log('hello from the top level code');
+
+--->
+hello from the top level code
+timer 1 finished
+immediate 1 finished
+I/O finished
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+const fs = require('fs');
+
+setTimeout(() => console.log('timer 1 finished'), 0 );
+setImmediate(() => console.log('immediate 1 finished'));
+
+fs.readFile("event_text.txt", () => {
+    console.log('I/O finished');
+
+    setTimeout(() => console.log('timer 2 finished'), 0 );
+    setTimeout(() => console.log('timer 3 finished'), 3000 );
+    setImmediate(() => console.log('immediate 2 finished'));
+
+});
+
+console.log('hello from the top level code');
+
+/* --->
+hello from the top level code
+timer 1 finished
+immediate 1 finished
+I/O finished
+immediate 2 finished
+timer 2 finished
+timer 3 finished
+*/
